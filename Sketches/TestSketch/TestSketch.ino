788a396d8de6fb6cce63dfef4d0618d0fda09277
desmond1999d <desmond1999d@mail.ru>
2018-11-18 22:13:46 +0300

    Refactored code

--- Sketches/TestSketch/TestSketch.ino
+++ Sketches/TestSketch/TestSketch.ino
@@ -1,23 +1,32 @@
-#include "FastLed.h"
-#define NUM_LEDS 30
+#include <FastLED.h> 
+
+#define NUM_LEDS 38
 
 CRGB leds[NUM_LEDS];
 
 #define PIN 13
+int lcount = 0; 
 
 void setup()
 {
-  FastLed.addLeds<WS2812, PIN, RGB>(leds, NUM_LEDS);
+  LEDS.addLeds<WS2812, PIN, RGB>(leds, NUM_LEDS);
 }
 
-void loop() 
-{
-  RGBLoop();
-  Strobe(0xff, 0xff, 0xff, 10, 50, 1000);
-  HalloweenEyes(0xff, 0x00, 0x00, 
-                              1, 4, 
-                              true, random(5,50), random(50,150), 
-                              random(1000, 10000));
+void set_color_led(int adex, int cred, int cgrn, int cblu) {
+  leds[adex].setRGB( cred, cgrn, cblu);
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+  leds[Pixel].r = red;
+  leds[Pixel].g = green;
+  leds[Pixel].b = blue;
+}
+
+void setAll(byte red, byte green, byte blue) {
+  for (int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue);
+  }
+  FastLED.show();
 }
 
 void RGBLoop(){
@@ -28,7 +37,7 @@ void RGBLoop(){
         case 1: setAll(0,k,0); break;
         case 2: setAll(0,0,k); break;
       }
-      showStrip();
+      FastLED.show();
       delay(3);
     }
     for(int k = 255; k >= 0; k--) { 
@@ -37,61 +46,68 @@ void RGBLoop(){
         case 1: setAll(0,k,0); break;
         case 2: setAll(0,0,k); break;
       }
-      showStrip();
+      FastLED.show();
       delay(3);
     }
   }
 }
 
-void Strobe(byte red, byte green, byte blue, int StrobeCount, int FlashDelay, int EndPause){
-  for(int j = 0; j < StrobeCount; j++) {
-    setAll(red,green,blue);
-    showStrip();
+void Strobe(byte red, byte green, byte blue, int StrobeCount, int FlashDelay, int EndPause) {
+  for (int j = 0; j < StrobeCount; j++) {
+    setAll(red, green, blue);
+    FastLED.show();
     delay(FlashDelay);
-    setAll(0,0,0);
-    showStrip();
+    setAll(0, 0, 0);
+    FastLED.show();
     delay(FlashDelay);
   }
- 
- delay(EndPause);
+
+  delay(EndPause);
 }
 
-void HalloweenEyes(byte red, byte green, byte blue, 
-                   int EyeWidth, int EyeSpace, 
-                   boolean Fade, int Steps, int FadeDelay,
-                   int EndPause){
-  randomSeed(analogRead(0));
-  
-  int i;
-  int StartPoint  = random( 0, NUM_LEDS - (2*EyeWidth) - EyeSpace );
-  int Start2ndEye = StartPoint + EyeWidth + EyeSpace;
-  
-  for(i = 0; i < EyeWidth; i++) {
-    setPixel(StartPoint + i, red, green, blue);
-    setPixel(Start2ndEye + i, red, green, blue);
+
+void pacman() {                                  //-m24-REALLY TERRIBLE PACMAN CHOMPING EFFECT
+  int s = int(NUM_LEDS / 4);
+  lcount++;
+  if (lcount > 5) {
+    lcount = 0;
   }
-  
-  showStrip();
-  
-  if(Fade==true) {
-    float r, g, b;
-  
-    for(int j = Steps; j >= 0; j--) {
-      r = j*(red/Steps);
-      g = j*(green/Steps);
-      b = j*(blue/Steps);
-      
-      for(i = 0; i < EyeWidth; i++) {
-        setPixel(StartPoint + i, r, g, b);
-        setPixel(Start2ndEye + i, r, g, b);
-      }
-      
-      showStrip();
-      delay(FadeDelay);
+  if (lcount == 0) {
+    for (int i = 0 ; i < NUM_LEDS; i++ ) {
+      set_color_led(i, 255, 255, 0);
     }
   }
-  
-  setAll(0,0,0); // Set all black
-  
-  delay(EndPause);
+  if (lcount == 1 || lcount == 5) {
+    for (int i = 0 ; i < NUM_LEDS; i++ ) {
+      set_color_led(i, 255, 255, 0);
+    }
+    leds[s].r = 0; leds[s].g = 0; leds[s].b = 0;
+  }
+  if (lcount == 2 || lcount == 4) {
+    for (int i = 0 ; i < NUM_LEDS; i++ ) {
+      set_color_led(i, 255, 255, 0);
+    }
+    leds[s - 1].r = 0; leds[s - 1].g = 0; leds[s - 1].b = 0;
+    leds[s].r = 0; leds[s].g = 0; leds[s].b = 0;
+    leds[s + 1].r = 0; leds[s + 1].g = 0; leds[s + 1].b = 0;
+  }
+  if (lcount == 3) {
+    for (int i = 0 ; i < NUM_LEDS; i++ ) {
+      set_color_led(i, 255, 255, 0);
+    }
+    leds[s - 2].r = 0; leds[s - 2].g = 0; leds[s - 2].b = 0;
+    leds[s - 1].r = 0; leds[s - 1].g = 0; leds[s - 1].b = 0;
+    leds[s].r = 0; leds[s].g = 0; leds[s].b = 0;
+    leds[s + 1].r = 0; leds[s + 1].g = 0; leds[s + 1].b = 0;
+    leds[s + 2].r = 0; leds[s + 2].g = 0; leds[s + 2].b = 0;
+  }
+  LEDS.show();
+  delay(1000);
+}
+
+void loop() 
+{
+  RGBLoop();
+  Strobe(0x88, 0x11, 0x22, 10, 500, 1000);
+  pacman();
 }
